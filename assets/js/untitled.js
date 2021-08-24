7e1f701f4d21fe8969cf45a13ec62ec770ee3ca8
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2021-08-24 01:56:04 -0500

    Día 2 navbar-carousel

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -1,15 +1,18 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const webBody = document.body;
+const menuOverlay = document.getElementById('cuerpo');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         webBody.classList.add('oh');
+        menuOverlay.classList.remove('dn');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         webBody.classList.remove('oh');
+        menuOverlay.classList.add('dn');
         menuOpen = false;
     }
 });
