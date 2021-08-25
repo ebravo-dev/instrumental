c8050182a7f77d7ebf0be9235b2d5ab8313dd1e4
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2021-08-24 23:35:54 -0500

    Día 3 Navbar+Carousel+Animations

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const webBody = document.body;
 const menuOverlay = document.getElementById('cuerpo');
+const homeClick = document.getElementsByClassName('home-click');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
@@ -16,3 +17,10 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+for (let index = 0; index < homeClick.length; index++) {
+    homeClick[index].addEventListener('click', () => {
+        window.location = '/';
+    });
+
+}
