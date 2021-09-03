851e08aa92f834a95a83c105793093e228331bd6
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2021-09-02 19:10:35 -0500

    Día 7,8,9

--- assets/js/untitled.js
+++ assets/js/untitled.js
@@ -4,6 +4,8 @@ const webBody = document.body;
 const menuOverlay = document.getElementById('cuerpo');
 const homeClick = document.getElementsByClassName('home-click');
 const navBar = document.getElementById('nav-bar');
+const topBar = document.getElementById('top-bar');
+const textsBar = document.getElementsByClassName('textb');
 let menuOpen = false;
 
 function _openMenu() {
@@ -35,14 +37,40 @@ for (let index = 0; index < homeClick.length; index++) {
 
 }
 
-window.addEventListener('resize', function (event) {
-    _closeMenu();
-}, true);
+// window.addEventListener('resize', function (event) {
+//     _closeMenu();
+// }, true);
 
 window.addEventListener('scroll', function (event) {
-    // if (window.scrollY > 0) {
-    //     navBar.classList.add('nvs');
-    // } else {
-    //     navBar.classList.remove('nvs');
-    // }
+    if (window.scrollY > 0) {
+        navBar.classList.add('nvs');
+        topBar.style.height = '0px';
+
+        for (let i = 0; i < textsBar.length; i++) {
+            textsBar[i].style.height = '80px';
+            textsBar[i].style.fontSize = '16pt';
+        }
+    } else {
+        navBar.classList.remove('nvs');
+        topBar.style.height = '8px';
+        for (let i = 0; i < textsBar.length; i++) {
+            textsBar[i].style.height = '100px';
+            textsBar[i].style.fontSize = '18pt';
+        }
+    }
 }, true);
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    effect: "fade",
+    speed: 1000,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
